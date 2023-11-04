a9d6ff56d38d99a81bd32a27bdf3eb4385b49e6e
Lucas Ritzdorf <42657792+LRitzdorf@users.noreply.github.com>
2023-11-04 01:02:10 -0600

    pwm: clean up formatting slightly

--- hw/pwm.vhd
+++ hw/pwm.vhd
@@ -12,28 +12,26 @@ use ieee.std_logic_unsigned.all;
 -- PWM controller interface
 entity PWM is
     generic (
-        SYS_CLKs_sec : uint -- Number of system clocks periods in one second
+        SYS_CLKs_sec : integer range 0 to 2**30 -- Number of system clocks periods in one second
     );
     port (
-        clk          : in  std_logic;             -- system clock
-        reset        : in  std_logic;             -- system reset (assume active high, change at top level if needed)
-        period       : in  unsigned(17 downto 0); -- PWM repetition period in milliseconds; Data type UQ10.7
-        duty_cycle   : in  unsigned(14 downto 0); -- PWM control word: [0 1]; Out-of-range values ignored and hard limited; Data type UQ2.12
-        pwm_out      : out std_logic              -- PWM output signal
+        clk        : in  std_logic;             -- system clock
+        reset      : in  std_logic;             -- system reset, active high
+        period     : in  unsigned(16 downto 0); -- PWM period in milliseconds, UQ10.7
+        duty_cycle : in  unsigned(13 downto 0); -- PWM duty cycle, UQ2.12, range [0 1] (out-of-range values saturate)
+        pwm_out    : out std_logic              -- PWM output signal
     );
 end entity;
 
 
 -- PWM controller functionality
 architecture PWM_Arch of PWM is
-
     -- Number of system clocks per millisecond (synthesis-time derived constant)
-    constant CLKS_PER_MS : uint := SYS_CLKs_sec / 1000;
+    constant CLKS_PER_MS : uint := to_unsigned(SYS_CLKs_sec / 1000, 32);
     -- Derived control values
     signal per_limit, duty_limit : uint;
     -- Counter
     signal count : uint;
-
 begin
 
     -- Derive control values
