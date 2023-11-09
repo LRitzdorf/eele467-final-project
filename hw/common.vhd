c9d127f798ff2d93c87f71b1cd1ac178540dcc2b
Lucas Ritzdorf <42657792+LRitzdorf@users.noreply.github.com>
2023-11-09 03:50:19 -0700

    common: implement clog2 function, for possible future use

--- hw/common.vhd
+++ hw/common.vhd
@@ -13,5 +13,22 @@ use ieee.numeric_std.all;
 package common is
 
     subtype uint is unsigned(31 downto 0);
+    function clog2 (x : positive) return natural;
 
 end package;
+
+
+package body common is
+
+    -- Reused from https://stackoverflow.com/a/12751341
+    function clog2 (x : positive) return natural is
+        variable i : natural;
+    begin
+        i := 0;
+        while (2**i < x) and i < 31 loop
+            i := i + 1;
+        end loop;
+        return i;
+    end function;
+
+end package body;
