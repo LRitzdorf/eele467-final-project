1ccc2ac603878b8fbb8d93b56e46fcef9ba44c3f
Lucas Ritzdorf <42657792+LRitzdorf@users.noreply.github.com>
2023-11-13 18:38:23 -0700

    pwm: expand testing, fix truncation bug at high periods

--- hw/pwm.vhd
+++ hw/pwm.vhd
@@ -28,18 +28,19 @@ architecture PWM_Arch of PWM is
     -- Number of system clocks per millisecond (synthesis-time derived constant)
     constant CLKS_PER_MS : natural := SYS_CLKs_sec / 1000;
     -- Derived control values
-    signal per_limit, duty_limit : natural;
+    signal per_limit  : unsigned(period'high-7+clog2(CLKS_PER_MS) downto 0);
+    signal duty_limit : unsigned(per_limit'high+duty_cycle'high-12 downto 0);
     -- Counter
-    signal count, next_count : natural;
+    signal count, next_count : unsigned(per_limit'high downto 0);
 begin
 
     -- Count up or reset, as appropriate
     count_gen : process (all) is
     begin
         if reset then
-            next_count <= 0;
+            next_count <= (others => '0');
         elsif (count >= per_limit - 1) or (per_limit = 0) then
-            next_count <= 0;
+            next_count <= (others => '0');
         else
             next_count <= count + 1;
         end if;
@@ -48,22 +49,22 @@ begin
     -- Use a basic counter to track progress through the PWM cycle
     pwm_gen : process (clk, reset) is
         -- Internal intermediate variable for control value updates
-        variable clks_per_period : natural;
+        variable clks_per_period : unsigned(period'high-7+clog2(CLKS_PER_MS) downto 0);
     begin
-        clks_per_period := to_integer(shift_right("*"(period, CLKS_PER_MS), 7));
+        clks_per_period := "*"(period, CLKS_PER_MS)(period'high+clog2(CLKS_PER_MS) downto 7+0);
 
         if reset then
             -- Reset counter
-            count <= 0;
+            count <= (others => '0');
             -- Update control values
             per_limit <= clks_per_period;
-            duty_limit <= to_integer(shift_right("*"(clks_per_period, duty_cycle), 12));
+            duty_limit <= "*"(clks_per_period, duty_cycle)(clks_per_period'high+duty_cycle'high downto 0+12);
         elsif rising_edge(clk) then
 
             -- Update control values
             if next_count = 0 then
                 per_limit <= clks_per_period;
-                duty_limit <= to_integer(shift_right("*"(clks_per_period, duty_cycle), 12));
+                duty_limit <= "*"(clks_per_period, duty_cycle)(clks_per_period'high+duty_cycle'high downto 0+12);
             end if;
             -- Update counter
             count <= next_count;
