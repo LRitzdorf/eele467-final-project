b09d0c912d61e2609f1f5e5fd6e1f1720c190279
Lucas Ritzdorf <42657792+LRitzdorf@users.noreply.github.com>
2023-11-14 20:05:23 -0700

    pwm(driver): create Makefile and register offset header

---
+++ linux/pwm/reg_offsets.h
@@ -0,0 +1,13 @@
+#ifndef REG_OFFSETS_H
+#define REG_OFFSETS_H
+
+// Define register offsets for the component
+#define REG0_PERIOD_OFFSET 0x0
+#define REG1_RED_OFFSET 0x4
+#define REG2_GREEN_OFFSET 0x8
+#define REG3_BLUE_OFFSET 0xC
+
+// Memory span of all registers (used or not) in the component
+#define SPAN 0x10
+
+#endif
