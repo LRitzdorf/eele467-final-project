fb7948bf4be5188713f2df4ee1a4f86dfef94377
Lucas Ritzdorf <42657792+LRitzdorf@users.noreply.github.com>
2023-11-28 22:48:47 -0700

    adc(driver): create Makefile and register offset header

---
+++ linux/adc/reg_offsets.h
@@ -0,0 +1,19 @@
+#ifndef REG_OFFSETS_H
+#define REG_OFFSETS_H
+
+// Define register offsets for the component
+#define REG_W_UPDATE_OFFSET 0x0
+#define REG_W_AUTO_UPDATE_OFFSET 0x4
+#define REG_R_CH0_OFFSET 0x00
+#define REG_R_CH1_OFFSET 0x04
+#define REG_R_CH2_OFFSET 0x08
+#define REG_R_CH3_OFFSET 0x0C
+#define REG_R_CH4_OFFSET 0x10
+#define REG_R_CH5_OFFSET 0x14
+#define REG_R_CH6_OFFSET 0x18
+#define REG_R_CH7_OFFSET 0x1C
+
+// Memory span of all registers (used or not) in the component
+#define SPAN 0x20
+
+#endif
