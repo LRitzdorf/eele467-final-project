2dcc25aa1597273d2688c5c073e9d36d8738748f
Lucas Ritzdorf <42657792+LRitzdorf@users.noreply.github.com>
2023-12-01 18:21:29 -0700

    sw(adc_control): add ADC-based PWM control script

---
+++ src/adc_control.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Basic controller script to set PWM duty cycles based on ADC readings
+# Lucas Ritzdorf
+# 12/01/2023
+# EELE 467
+
+# Configuration variables
+ADC_PATH=/sys/class/misc/adc_controller
+PWM_PATH=/sys/class/misc/hps_multi_pwm
+
+echo "Control loop running; interrupt to exit..."
+# Main control loop
+while true
+do
+    cat "$ADC_PATH"/channel_0 > "$PWM_PATH"/duty_cycle_1
+    cat "$ADC_PATH"/channel_1 > "$PWM_PATH"/duty_cycle_2
+    cat "$ADC_PATH"/channel_2 > "$PWM_PATH"/duty_cycle_3
+    sleep 0.1
+done
