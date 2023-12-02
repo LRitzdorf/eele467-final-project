77e68141e32ad5b979dba1d33246c7fa94eb4ab4
Lucas Ritzdorf <42657792+LRitzdorf@users.noreply.github.com>
2023-12-01 18:23:00 -0700

    sw(adc_control): make loop delay configurable

--- src/adc_control.sh
+++ src/adc_control.sh
@@ -8,6 +8,7 @@
 # Configuration variables
 ADC_PATH=/sys/class/misc/adc_controller
 PWM_PATH=/sys/class/misc/hps_multi_pwm
+LOOP_DELAY=0.1
 
 echo "Control loop running; interrupt to exit..."
 # Main control loop
@@ -16,5 +17,5 @@ do
     cat "$ADC_PATH"/channel_0 > "$PWM_PATH"/duty_cycle_1
     cat "$ADC_PATH"/channel_1 > "$PWM_PATH"/duty_cycle_2
     cat "$ADC_PATH"/channel_2 > "$PWM_PATH"/duty_cycle_3
-    sleep 0.1
+    sleep "$LOOP_DELAY"
 done
