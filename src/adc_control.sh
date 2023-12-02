5fb9c264ea7609d2f3a763c4a0a96b0cc567a363
Lucas Ritzdorf <42657792+LRitzdorf@users.noreply.github.com>
2023-12-01 19:13:33 -0700

    sw(adc_control): also initialize PWM period

--- src/adc_control.sh
+++ src/adc_control.sh
@@ -8,10 +8,13 @@
 # Configuration variables
 ADC_PATH=/sys/class/misc/adc_controller
 PWM_PATH=/sys/class/misc/hps_multi_pwm
+PERIOD=0x100 # 2ms
 LOOP_DELAY=0.1
 
-echo "Control loop running; interrupt to exit..."
+# Initialization
+echo "$PERIOD" > "$PWM_PATH"/period
 # Main control loop
+echo "Control loop running; interrupt to exit..."
 while true
 do
     cat "$ADC_PATH"/channel_0 > "$PWM_PATH"/duty_cycle_1
@@ -19,3 +22,5 @@ do
     cat "$ADC_PATH"/channel_2 > "$PWM_PATH"/duty_cycle_3
     sleep "$LOOP_DELAY"
 done
+# Cleanup
+echo 0 > "$PWM_PATH"/period
