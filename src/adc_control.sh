8b75fd15bb7a083d16bd2e2d35f5ec3ee050d402
Lucas Ritzdorf <42657792+LRitzdorf@users.noreply.github.com>
2023-12-02 18:11:35 -0700

    sw(adc_control): check System ID before running
    This ensures that the correct FPGA hardware is present.

--- src/adc_control.sh
+++ src/adc_control.sh
@@ -6,10 +6,42 @@
 # EELE 467
 
 # Configuration variables
+SYSID_VERSION=0x3ADC37ED
 ADC_PATH=/sys/class/misc/adc_controller
 PWM_PATH=/sys/class/misc/hps_multi_pwm
 PERIOD=0x100 # 2ms
 LOOP_DELAY=0.1
+
+
+# Check System ID
+
+# Scan for valid device files
+shopt -s nullglob  # Return empty list if glob fails to match
+sysids=( /sys/bus/platform/devices/*.sysid/sysid/id )
+if [ "${#sysids[@]}" -lt 1 ]
+then
+    echo "No System ID device files found!"
+    return 1
+fi
+
+# Check device files for matching ID
+sysid_match=false
+for id in "${sysids[@]}"
+do
+    if [ $(< $id) -eq $(printf "%d" "$SYSID_VERSION") ]
+    then
+        sysid_match=true
+        break
+    fi
+done
+
+# Exit if nothing matched
+if [ ! $sysid_match ]
+then
+    printf "No matching System ID found! (Expected 0x%X)\n" "$SYSID_VERSION"
+    return 2
+fi
+
 
 # Initialization
 echo "$PERIOD" > "$PWM_PATH"/period
